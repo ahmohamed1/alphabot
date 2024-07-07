d0425bc48d362b2e2a330f87732b818f304800e0
Abdulla <abdll@DESKTOP-31TP4IV>
2024-07-07 12:42:07 +0300

    create new pkg utils and create new node for safety stop

--- alphabot_controller/launch/joystick_teleop.launch.py
+++ alphabot_controller/launch/joystick_teleop.launch.py
@@ -3,6 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
 
 
@@ -21,9 +22,26 @@ def generate_launch_description():
         parameters=[os.path.join(get_package_share_directory("alphabot_controller"), "config", "joy_config.yaml")]
     )
 
+    alphabot_controller_package = get_package_share_directory("alphabor_controller")
+
+    twist_mux_launch = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("twist_mux"),
+            "launch",
+            "twist_mux_launch.py"
+        ),
+        launch_arguments={
+            "cmd_vel_out": "alphabot_controller/cmd_vel_unstamped",
+            "config_topics": os.path.join(alphabot_controller_package,"config","twist_mux_topic.yaml"),
+            "config_locks":os.path.join(alphabot_controller_package,"config","twist_mux_lock.yaml"),
+            "config_joy":os.path.join(alphabot_controller_package,"config","twist_mux_joy.yaml"),
+        }.items()
+    )
+
     return LaunchDescription(
         [
             joy_teleop,
-            joy_node
+            joy_node,
+            twist_mux_launch
         ]
     )
