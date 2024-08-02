d407aba4bc79bdccf40a2f9b3a04cb045d5758c7
ahmohamed1 <abdll1@hotmail.com>
2024-08-02 17:21:06 +0300

    add sources of the code

--- alphabot_utils/src/robot_drive.cpp
+++ alphabot_utils/src/robot_drive.cpp
@@ -1,3 +1,9 @@
+'''
+This code based on https://wiki.ros.org/pr2_controllers/Tutorials/Using%20the%20base%20controller%20with%20odometry%20and%20transform%20information
+
+'''
+
+
 #include <iostream>
 #include <memory>
 #include <cmath>
